b2d57d32caa5515a17febb0e39a3f50b1a284e3d
Sam Fry <benolot@gmail.com>
2017-02-04 19:28:35 +0000

    Updated the readme and added gulp and package files.

---
+++ gulpfile.js
@@ -0,0 +1,62 @@
+var gulp = require('gulp');
+var plumber = require('gulp-plumber');
+var rename = require('gulp-rename');
+var sourcemaps = require('gulp-sourcemaps');
+var sass = require('gulp-sass');
+var csslint = require('gulp-csslint');
+var autoPrefixer = require('gulp-autoprefixer');
+//if node version is lower than v.0.1.2
+require('es6-promise').polyfill();
+var cssComb = require('gulp-csscomb');
+var cmq = require('gulp-merge-media-queries');
+var cleanCss = require('gulp-clean-css');
+var concat = require('gulp-concat');
+gulp.task('sass',function(){
+    gulp.src(['sass//**/*.sass'])
+        .pipe(plumber({
+            handleError: function (err) {
+                console.log(err);
+                this.emit('end');
+            }
+        }))
+        .pipe(sourcemaps.init())
+        .pipe(sass())
+        .pipe(autoPrefixer())
+        .pipe(cssComb())
+        .pipe(cmq({log:true}))
+        .pipe(csslint())
+        .pipe(csslint.reporter())
+        .pipe(concat('compiled.css'))
+        .pipe(gulp.dest('css/compiled'))
+        .pipe(rename({
+            suffix: '.min'
+        }))
+        .pipe(cleanCss())
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('css/compiled'))
+});
+gulp.task('js',function(){
+    gulp.src(['js/src/**/*.js'])
+        .pipe(plumber({
+            handleError: function (err) {
+                console.log(err);
+                this.emit('end');
+            }
+        }))
+        .pipe(gulp.dest('js/dist'))
+});
+gulp.task('html',function(){
+    gulp.src(['html/**/*.html'])
+        .pipe(plumber({
+            handleError: function (err) {
+                console.log(err);
+                this.emit('end');
+            }
+        }))
+        .pipe(gulp.dest('./'))
+});
+gulp.task('default',function(){
+    gulp.watch('js/src/**/*.js',['js']);
+    gulp.watch('sass//**/*.sass',['sass']);
+    gulp.watch('html/**/*.html',['html']);
+});
